efeda30f7848ba92e4cc8a5ec1498387129824cf
Diana Verallo <001649@lrmc.com.ph>
2018-01-10 16:17:57 +0800

    Add project files.

---
+++ CoolBreeze/CoolBreeze/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CoolBreeze
+{
+	public partial class App : Application
+	{
+	    public static MainViewModel ViewModel;
+	    public static string RegistrationCode = "XBWWA1A0D6CC1D5C77B239BC0B274844";
+
+        public App ()
+		{
+			InitializeComponent();
+
+            //MainPage = new CoolBreeze.MainPage();
+            //MainPage = new CoolBreeze.StartPage();
+		    MainPage = new NavigationPage(new CoolBreeze.StartPage());
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ CoolBreeze/CoolBreeze/StartPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CoolBreeze
+{
+    public partial class StartPage : TabbedPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            InitializeAppAsync();
+            base.OnAppearing();
+        }
+
+        private void InitializeAppAsync()
+        {
+            //DateTime badDateTime = DateTime.Parse("This is not a date or time"); //use to demo crash report transmitted to AppCenter
+            if (App.ViewModel == null) App.ViewModel = new MainViewModel();
+        }
+
+        private async void ViewForecastClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ForecastPage());
+        }
+    }
+}
